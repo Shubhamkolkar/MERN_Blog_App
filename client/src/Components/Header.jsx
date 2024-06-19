fc190034f6e4baf84e713e6ad95524d4d66e3a45
Shubham Kolkar <shubhamkolkar03@gmail.com>
2024-06-19 19:21:27 +0530

    changes in header

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+// import { IoMoonOutline } from "react-icons/io5";
+// <IoMoonOutline />
+{/* <IoMoonSharp /> */}
 
 const Header = () => {
+
   return (
     <>
       <nav className="bg-slate-200 p-4 flex justify-between items-center">
@@ -13,13 +18,20 @@ const Header = () => {
         </div>
 
         {/* Search Field and Navigation Links */}
-        <div className="flex-grow flex justify-center gap-2 items-center"> {/* Changed justify-between to justify-center */}
+        <div className="flex-grow flex justify-center gap-8 items-center"> 
           {/* Search Field */}
-          <input
-            type="text"
-            placeholder="Search"
-            className="px-3 py-1 rounded-lg focus:outline-none"
-          />
+          <form>
+            <div className="relative flex items-center">
+              <input
+                type="text"
+                className="px-3 py-1 rounded-lg focus:outline-none "
+                placeholder="Search"
+              />
+              <div className="absolute right-0 mr-2 text-gray-400 hidden lg:inline">
+                <AiOutlineSearch />
+              </div>
+            </div>
+          </form>
 
           {/* Navigation Links */}
           <ul className="flex items-center space-x-4">
@@ -37,9 +49,6 @@ const Header = () => {
 
         {/* Dark Mode and Sign In Buttons */}
         <div className="flex items-center">
-          <button className="bg-gray-700 hover:bg-gray-600 text-white px-3 py-1 rounded-full mr-2">
-            Dark Mode
-          </button>
           <button className="bg-blue-500 hover:bg-blue-700 text-white px-3 py-1 rounded-lg">
             Sign In
           </button>
