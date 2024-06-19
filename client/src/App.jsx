017512d7fd8773dedfe40b0865d8cefd163546a8
Shubham Kolkar <shubhamkolkar03@gmail.com>
2024-06-19 17:25:13 +0530

    Added Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Route,Routes} from 'react-router-dom'
+import Home from './Pages/Home'
+import Singin from './Pages/Singin'
+import Dashboard from './Pages/Dashboard'
+import About from './Pages/About'
+import Login from './Pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/ddashboard' element={<Dashboard/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signin' element={<Singin/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
