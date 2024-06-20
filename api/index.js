5b3eea6fcc487d454c40f59675b8772d5b4bb81a
Shubham Kolkar <shubhamkolkar03@gmail.com>
2024-06-20 23:01:56 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import { dotenv } from 'dotenv'
+
+dotenv.config();
 
 const app = express()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connection sucessfull")
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.get('/',  (req, res)=> {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log("server started at port" + "3000")
+})
