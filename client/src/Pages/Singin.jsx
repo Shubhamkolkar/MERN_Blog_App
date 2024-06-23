c5125485c772b241145bbdf001aeb1c66b3558a0
Shubham Kolkar <shubhamkolkar03@gmail.com>
2024-06-23 16:44:52 +0530

    Added Redux ToolKit

--- client/src/Pages/Singin.jsx
+++ client/src/Pages/Singin.jsx
@@ -1,6 +1,11 @@
-
 import React, { useState } from 'react';
-import { useNavigation, useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice.js';
 
 const Singin = () => {
   const [formData, setFormData] = useState({
@@ -9,72 +14,80 @@ const Singin = () => {
   });
 
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please Fill Out The Details")
+      return dispatch(signInFailure('Please fill all the fields'));
     }
-    const trimmedFormData = {
-      ...formData
-    };
 
-    setLoading(true);
     try {
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(trimmedFormData)
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
-      setLoading(false);
-      if (res.ok) {
-        navigate('/');
-      } else {
-        setErrorMessage(data.message || 'Something went wrong!');
-      }
-    } catch (error) {
-      setLoading(false);
-      setErrorMessage('Something went wrong!');
-    }
-    // console.log(formData); // You can handle form submission here
-    setFormData({
-      email: '',
-      password: ''
-    });
 
+      if (!res.ok || data.success === false) {
+        throw new Error(data.message || 'Sign in failed');
+      }
+
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    } finally {
+      setFormData({
+        email: '',
+        password: ''
+      });
+    }
   };
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-
       <Link to='/' className='font-bold text-4xl text-indigo-600 mb-8'>
         devBlog
       </Link>
 
-
-
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h2 className="text-2xl font-bold mb-4">Sign In</h2>
         <form onSubmit={handleSubmit}>
-
           <div className="mb-4">
-            <input type="email" id="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} className="mt-1 block w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 block w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
           </div>
           <div className="mb-6">
-            <input type="password" id="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} className="mt-1 block w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              className="mt-1 block w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
           </div>
-          <button type="submit" className="w-full bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700" disabled={loading}>
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700"
+            disabled={loading}
+          >
             {loading ? 'Signing In...' : 'Sign In'}
           </button>
           {errorMessage && <div className="mt-4 text-red-500">{errorMessage}</div>}
@@ -90,4 +103,4 @@ const Singin = () => {
   );
 }
 
-export default Singin
+export default Singin;
